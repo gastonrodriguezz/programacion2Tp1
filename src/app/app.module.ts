0a764e7eed2bc2bde7929d6ba45e2ee5fb0bd226
gastonrodriguezz <lpmgaston@gmail.com>
2020-09-19 21:57:04 -0300

    material v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
 import { ListadosucursalesComponent } from './body/listadosucursales/listadosucursales.component';
@@ -14,15 +13,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MyLineChartComponent } from './graficos/my-line-chart/my-line-chart.component';
 import { LinegraphComponent } from './graficos/linegraph/linegraph.component';
 import {MatTableModule} from '@angular/material/table';
-import { TablaComponent } from './tabla/tabla.component';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     BodyComponent,
     FooterComponent,
     ListadosucursalesComponent,
@@ -30,9 +26,6 @@ import { MatSortModule } from '@angular/material/sort';
     HomeComponent,
     MyLineChartComponent,
     SucursalesComponent,
-    TablaComponent,
-   // MatTableModule,
-    
     // ArbolNavidadComponent,
   ],
   imports: [
@@ -44,7 +37,7 @@ import { MatSortModule } from '@angular/material/sort';
     BrowserAnimationsModule,
     MatTableModule,
     MatPaginatorModule,
-    MatSortModule
+    MatSortModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
